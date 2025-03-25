93bf74cc5e9655d88a51e334be8f4cf593ab300c
李静安 <3081309224@qq.com>
2025-03-26 01:31:37 +0800

    提交

---
+++ LiuLiuTools/Info/DeviceInfo/ComputerInfo.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Linq;
+using System.Management;
+using System.Threading;
+
+
+namespace LiuLiuTools.Info.DeviceInfo
+{
+    public class ComputerInfo
+    {
+        #region 懒加载实现
+        private static readonly Lazy<ComputerInfo> _computerInfo = new Lazy<ComputerInfo>(() => new ComputerInfo(), LazyThreadSafetyMode.ExecutionAndPublication);
+        public static ComputerInfo Instance => _computerInfo.Value;
+
+        //私有构造函数（防止外部实例化）
+        private ComputerInfo() 
+        {
+            CpuID = GetCpuID();
+            DiskID = GetDiskID();
+            LoginUserName = GetUserName();
+            SystemType = GetSystemType();
+            TotalPhysicalMemory = GetTotalPhysicalMemory();
+            ComputerName = GetComputerName();
+        }
+        #endregion
+
+        #region 字段
+        public string CpuID;
+        public string DiskID;
+        public string LoginUserName;
+        public string ComputerName;
+        public string SystemType;
+        /// <summary>
+        /// 单位：M 
+        /// </summary>
+        public string TotalPhysicalMemory;
+        #endregion
+
+        public string GetCpuID()
+        {
+            try
+            {
+                //获取CPU序列号代码 
+                string cpuInfo = "";//cpu序列号 
+                ManagementClass mc = new ManagementClass("Win32_Processor");
+                ManagementObjectCollection moc = mc.GetInstances();
+                foreach (ManagementObject mo in moc)
+                {
+                    cpuInfo = mo.Properties["ProcessorId"].Value.ToString();
+                }
+                moc.Dispose();
+                mc.Dispose();
+                return cpuInfo;
+            }
+            catch
+            {
+                return "unknow";
+            }
+
+        }
+
+        public string GetDiskID()
+        {
+            try
+            {
+                //获取硬盘ID 
+                String HDid = "";
+                ManagementClass mc = new ManagementClass("Win32_DiskDrive");
+                ManagementObjectCollection moc = mc.GetInstances();
+                foreach (ManagementObject mo in moc)
+                {
+                    HDid = (string)mo.Properties["Model"].Value;
+                }
+                moc = null;
+                mc = null;
+                return HDid;
+            }
+            catch
+            {
+                return "unknow";
+            }
+            finally
+            {
+            }
+
+        }
+
+        /// <summary> 
+        /// 操作系统的登录用户名 
+        /// </summary> 
+        /// <returns></returns> 
+        public string GetUserName()
+        {
+            try
+            {
+                string st = "";
+                ManagementClass mc = new ManagementClass("Win32_ComputerSystem");
+                ManagementObjectCollection moc = mc.GetInstances();
+                foreach (ManagementObject mo in moc)
+                {
+
+                    st = mo["UserName"].ToString();
+
+                }
+                moc = null;
+                mc = null;
+                return st;
+            }
+            catch
+            {
+                return "unknow";
+            }
+            finally
+            {
+            }
+
+        }
+
+        /// <summary> 
+        /// PC类型 
+        /// </summary> 
+        /// <returns></returns> 
+        public string GetSystemType()
+        {
+            try
+            {
+                string st = "";
+                ManagementClass mc = new ManagementClass("Win32_ComputerSystem");
+                ManagementObjectCollection moc = mc.GetInstances();
+                foreach (ManagementObject mo in moc)
+                {
+
+                    st = mo["SystemType"].ToString();
+
+                }
+                moc = null;
+                mc = null;
+                return st;
+            }
+            catch
+            {
+                return "unknow";
+            }
+            finally
+            {
+            }
+
+        }
+
+        /// <summary> 
+        /// 物理内存 
+        /// </summary> 
+        /// <returns></returns> 
+        public string GetTotalPhysicalMemory()
+        {
+            try
+            {
+
+                string st = "";
+                ManagementClass mc = new ManagementClass("Win32_ComputerSystem");
+                ManagementObjectCollection moc = mc.GetInstances();
+                foreach (ManagementObject mo in moc)
+                {
+
+                    st = mo["TotalPhysicalMemory"].ToString();
+
+                }
+                moc = null;
+                mc = null;
+                return st;
+            }
+            catch
+            {
+                return "unknow";
+            }
+            finally
+            {
+            }
+        }
+
+        /// <summary> 
+        ///  
+        /// </summary> 
+        /// <returns></returns> 
+        public string GetComputerName()
+        {
+            try
+            {
+                return System.Environment.GetEnvironmentVariable("ComputerName");
+            }
+            catch
+            {
+                return "unknow";
+            }
+            finally
+            {
+            }
+        }
+    }
+}
