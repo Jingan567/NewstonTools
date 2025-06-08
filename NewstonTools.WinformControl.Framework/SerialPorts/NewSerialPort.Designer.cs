f60d32cb9e7dbf8b9b73c6b3f2c4a6255dc057f4
李静安 <3081309224@qq.com>
2025-06-08 15:01:06 +0800

    提交

--- NewstonTools.WinformControl.Framework/SerialPorts/NewSerialPort.Designer.cs
+++ NewstonTools.WinformControl.Framework/SerialPorts/NewSerialPort.Designer.cs
@@ -28,17 +28,259 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.uiButton1 = new Sunny.UI.UIButton();
+            this.uiComboBox1 = new Sunny.UI.UIComboBox();
+            this.uiLabel1 = new Sunny.UI.UILabel();
+            this.uiLabel2 = new Sunny.UI.UILabel();
+            this.uiTitlePanel1 = new Sunny.UI.UITitlePanel();
+            this.uiComboBox2 = new Sunny.UI.UIComboBox();
+            this.uiButton2 = new Sunny.UI.UIButton();
+            this.uiLabel5 = new Sunny.UI.UILabel();
+            this.uiLabel4 = new Sunny.UI.UILabel();
+            this.uiLabel3 = new Sunny.UI.UILabel();
+            this.uiComboBox3 = new Sunny.UI.UIComboBox();
+            this.uiComboBox4 = new Sunny.UI.UIComboBox();
+            this.uiComboBox5 = new Sunny.UI.UIComboBox();
+            this.uiTitlePanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // uiButton1
+            // 
+            this.uiButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiButton1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton1.Location = new System.Drawing.Point(107, 406);
+            this.uiButton1.MinimumSize = new System.Drawing.Size(60, 20);
+            this.uiButton1.Name = "uiButton1";
+            this.uiButton1.Radius = 1;
+            this.uiButton1.Size = new System.Drawing.Size(245, 58);
+            this.uiButton1.TabIndex = 0;
+            this.uiButton1.Text = "测试连接";
+            this.uiButton1.TipsFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton1.Click += new System.EventHandler(this.uiButton1_Click);
+            // 
+            // uiComboBox1
+            // 
+            this.uiComboBox1.DataSource = null;
+            this.uiComboBox1.FillColor = System.Drawing.Color.White;
+            this.uiComboBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiComboBox1.ItemHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(200)))), ((int)(((byte)(255)))));
+            this.uiComboBox1.ItemSelectForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.uiComboBox1.Location = new System.Drawing.Point(368, 93);
+            this.uiComboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiComboBox1.MinimumSize = new System.Drawing.Size(63, 0);
+            this.uiComboBox1.Name = "uiComboBox1";
+            this.uiComboBox1.Padding = new System.Windows.Forms.Padding(0, 0, 30, 2);
+            this.uiComboBox1.Size = new System.Drawing.Size(275, 44);
+            this.uiComboBox1.SymbolSize = 24;
+            this.uiComboBox1.TabIndex = 1;
+            this.uiComboBox1.Text = "uiComboBox1";
+            this.uiComboBox1.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiComboBox1.Watermark = "";
+            // 
+            // uiLabel1
+            // 
+            this.uiLabel1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiLabel1.Location = new System.Drawing.Point(103, 99);
+            this.uiLabel1.MinimumSize = new System.Drawing.Size(140, 30);
+            this.uiLabel1.Name = "uiLabel1";
+            this.uiLabel1.Size = new System.Drawing.Size(213, 41);
+            this.uiLabel1.TabIndex = 2;
+            this.uiLabel1.Text = "端口号：";
+            this.uiLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // uiLabel2
+            // 
+            this.uiLabel2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiLabel2.Location = new System.Drawing.Point(103, 153);
+            this.uiLabel2.MinimumSize = new System.Drawing.Size(140, 30);
+            this.uiLabel2.Name = "uiLabel2";
+            this.uiLabel2.Size = new System.Drawing.Size(213, 41);
+            this.uiLabel2.TabIndex = 3;
+            this.uiLabel2.Text = "波特率：";
+            this.uiLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // uiTitlePanel1
+            // 
+            this.uiTitlePanel1.Controls.Add(this.uiComboBox5);
+            this.uiTitlePanel1.Controls.Add(this.uiComboBox4);
+            this.uiTitlePanel1.Controls.Add(this.uiComboBox3);
+            this.uiTitlePanel1.Controls.Add(this.uiComboBox2);
+            this.uiTitlePanel1.Controls.Add(this.uiButton2);
+            this.uiTitlePanel1.Controls.Add(this.uiLabel5);
+            this.uiTitlePanel1.Controls.Add(this.uiLabel4);
+            this.uiTitlePanel1.Controls.Add(this.uiLabel3);
+            this.uiTitlePanel1.Controls.Add(this.uiButton1);
+            this.uiTitlePanel1.Controls.Add(this.uiComboBox1);
+            this.uiTitlePanel1.Controls.Add(this.uiLabel1);
+            this.uiTitlePanel1.Controls.Add(this.uiLabel2);
+            this.uiTitlePanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.uiTitlePanel1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTitlePanel1.Location = new System.Drawing.Point(0, 0);
+            this.uiTitlePanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTitlePanel1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiTitlePanel1.Name = "uiTitlePanel1";
+            this.uiTitlePanel1.Padding = new System.Windows.Forms.Padding(1, 35, 1, 1);
+            this.uiTitlePanel1.ShowText = false;
+            this.uiTitlePanel1.Size = new System.Drawing.Size(826, 718);
+            this.uiTitlePanel1.TabIndex = 4;
+            this.uiTitlePanel1.Text = "串口配置";
+            this.uiTitlePanel1.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // uiComboBox2
+            // 
+            this.uiComboBox2.DataSource = null;
+            this.uiComboBox2.FillColor = System.Drawing.Color.White;
+            this.uiComboBox2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiComboBox2.ItemHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(200)))), ((int)(((byte)(255)))));
+            this.uiComboBox2.ItemSelectForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.uiComboBox2.Location = new System.Drawing.Point(368, 149);
+            this.uiComboBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiComboBox2.MinimumSize = new System.Drawing.Size(63, 0);
+            this.uiComboBox2.Name = "uiComboBox2";
+            this.uiComboBox2.Padding = new System.Windows.Forms.Padding(0, 0, 30, 2);
+            this.uiComboBox2.Size = new System.Drawing.Size(275, 44);
+            this.uiComboBox2.SymbolSize = 24;
+            this.uiComboBox2.TabIndex = 2;
+            this.uiComboBox2.Text = "uiComboBox2";
+            this.uiComboBox2.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiComboBox2.Watermark = "";
+            // 
+            // uiButton2
+            // 
+            this.uiButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiButton2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton2.Location = new System.Drawing.Point(398, 406);
+            this.uiButton2.MinimumSize = new System.Drawing.Size(60, 20);
+            this.uiButton2.Name = "uiButton2";
+            this.uiButton2.Radius = 1;
+            this.uiButton2.Size = new System.Drawing.Size(245, 58);
+            this.uiButton2.TabIndex = 7;
+            this.uiButton2.Text = "保存配置";
+            this.uiButton2.TipsFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton2.Click += new System.EventHandler(this.uiButton2_Click);
+            // 
+            // uiLabel5
+            // 
+            this.uiLabel5.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiLabel5.Location = new System.Drawing.Point(103, 315);
+            this.uiLabel5.MinimumSize = new System.Drawing.Size(140, 30);
+            this.uiLabel5.Name = "uiLabel5";
+            this.uiLabel5.Size = new System.Drawing.Size(213, 38);
+            this.uiLabel5.TabIndex = 6;
+            this.uiLabel5.Text = "奇数校验位：";
+            this.uiLabel5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // uiLabel4
+            // 
+            this.uiLabel4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiLabel4.Location = new System.Drawing.Point(103, 261);
+            this.uiLabel4.MinimumSize = new System.Drawing.Size(140, 30);
+            this.uiLabel4.Name = "uiLabel4";
+            this.uiLabel4.Size = new System.Drawing.Size(213, 41);
+            this.uiLabel4.TabIndex = 5;
+            this.uiLabel4.Text = "停止位：";
+            this.uiLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // uiLabel3
+            // 
+            this.uiLabel3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiLabel3.Location = new System.Drawing.Point(103, 207);
+            this.uiLabel3.MinimumSize = new System.Drawing.Size(140, 30);
+            this.uiLabel3.Name = "uiLabel3";
+            this.uiLabel3.Size = new System.Drawing.Size(213, 41);
+            this.uiLabel3.TabIndex = 4;
+            this.uiLabel3.Text = "数据位：";
+            this.uiLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // uiComboBox3
+            // 
+            this.uiComboBox3.DataSource = null;
+            this.uiComboBox3.FillColor = System.Drawing.Color.White;
+            this.uiComboBox3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiComboBox3.ItemHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(200)))), ((int)(((byte)(255)))));
+            this.uiComboBox3.ItemSelectForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.uiComboBox3.Location = new System.Drawing.Point(368, 205);
+            this.uiComboBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiComboBox3.MinimumSize = new System.Drawing.Size(63, 0);
+            this.uiComboBox3.Name = "uiComboBox3";
+            this.uiComboBox3.Padding = new System.Windows.Forms.Padding(0, 0, 30, 2);
+            this.uiComboBox3.Size = new System.Drawing.Size(275, 44);
+            this.uiComboBox3.SymbolSize = 24;
+            this.uiComboBox3.TabIndex = 3;
+            this.uiComboBox3.Text = "uiComboBox3";
+            this.uiComboBox3.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiComboBox3.Watermark = "";
+            // 
+            // uiComboBox4
+            // 
+            this.uiComboBox4.DataSource = null;
+            this.uiComboBox4.FillColor = System.Drawing.Color.White;
+            this.uiComboBox4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiComboBox4.ItemHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(200)))), ((int)(((byte)(255)))));
+            this.uiComboBox4.ItemSelectForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.uiComboBox4.Location = new System.Drawing.Point(368, 261);
+            this.uiComboBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiComboBox4.MinimumSize = new System.Drawing.Size(63, 0);
+            this.uiComboBox4.Name = "uiComboBox4";
+            this.uiComboBox4.Padding = new System.Windows.Forms.Padding(0, 0, 30, 2);
+            this.uiComboBox4.Size = new System.Drawing.Size(275, 44);
+            this.uiComboBox4.SymbolSize = 24;
+            this.uiComboBox4.TabIndex = 4;
+            this.uiComboBox4.Text = "uiComboBox4";
+            this.uiComboBox4.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiComboBox4.Watermark = "";
+            // 
+            // uiComboBox5
+            // 
+            this.uiComboBox5.DataSource = null;
+            this.uiComboBox5.FillColor = System.Drawing.Color.White;
+            this.uiComboBox5.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiComboBox5.ItemHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(200)))), ((int)(((byte)(255)))));
+            this.uiComboBox5.ItemSelectForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.uiComboBox5.Location = new System.Drawing.Point(368, 317);
+            this.uiComboBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiComboBox5.MinimumSize = new System.Drawing.Size(63, 0);
+            this.uiComboBox5.Name = "uiComboBox5";
+            this.uiComboBox5.Padding = new System.Windows.Forms.Padding(0, 0, 30, 2);
+            this.uiComboBox5.Size = new System.Drawing.Size(275, 44);
+            this.uiComboBox5.SymbolSize = 24;
+            this.uiComboBox5.TabIndex = 5;
+            this.uiComboBox5.Text = "uiComboBox5";
+            this.uiComboBox5.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiComboBox5.Watermark = "";
             // 
             // NewSerialPort
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.uiTitlePanel1);
+            this.MinimumSize = new System.Drawing.Size(60, 10);
             this.Name = "NewSerialPort";
-            this.Size = new System.Drawing.Size(629, 384);
+            this.Size = new System.Drawing.Size(826, 718);
+            this.Text = "";
+            this.uiTitlePanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Sunny.UI.UIButton uiButton1;
+        private Sunny.UI.UIComboBox uiComboBox1;
+        private Sunny.UI.UILabel uiLabel1;
+        private Sunny.UI.UILabel uiLabel2;
+        private Sunny.UI.UITitlePanel uiTitlePanel1;
+        private Sunny.UI.UILabel uiLabel4;
+        private Sunny.UI.UILabel uiLabel3;
+        private Sunny.UI.UILabel uiLabel5;
+        private Sunny.UI.UIButton uiButton2;
+        private Sunny.UI.UIComboBox uiComboBox2;
+        private Sunny.UI.UIComboBox uiComboBox5;
+        private Sunny.UI.UIComboBox uiComboBox4;
+        private Sunny.UI.UIComboBox uiComboBox3;
     }
 }
--- NewstonTools.WinformControl.Framework/SerialPorts/NewSerialPort.cs
+++ NewstonTools.WinformControl.Framework/SerialPorts/NewSerialPort.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,7 +19,66 @@ namespace NewstonTools.WinformControl.Framework.SerialPorts
         {
             InitializeComponent();
         }
+
         [Browsable(true)]
-        public string hello { get; set; }
+        [Description("配置文件保存地址，需配置")]
+        public string ConfigPath { get; set; }//属性不能私有
+
+        [Browsable(true)]
+        [Description("发送的串口命令，需配置")]
+        public string SendCommand { get; set; }
+
+
+        /// <summary>
+        /// 数据接收事件
+        /// </summary>
+        [Browsable(true)]
+        public event Action<List<byte>> DataReceived;
+        /// <summary>
+        /// 串口接收错误数据处理程序
+        /// </summary>
+        [Browsable(true)]
+        public event SerialErrorReceivedEventHandler ErrorReceived;
+
+        private SerialPort serialportClient { get; set; }
+
+        /// <summary>
+        /// 测试连接
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uiButton1_Click(object sender, EventArgs e)
+        {
+            serialportClient = new SerialPort();
+            serialportClientConfigChangeFromUI();
+
+        }                    
+                          
+        /// <summary>
+        /// 保存配置
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>DataBits
+        private void uiButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 使用配置初始化SerialPort
+        /// </summary>
+        private void InitUseConfig()
+        {
+
+        }
+
+        private void serialportClientConfigChangeFromUI()
+        {
+            serialportClient.PortName = uiComboBox1.SelectedText;
+            serialportClient.BaudRate = (int)uiComboBox2.SelectedValue;
+            serialportClient.DataBits = (int)uiComboBox3.SelectedValue;
+            serialportClient.StopBits = (StopBits)uiComboBox4.SelectedValue;
+            serialportClient.Parity = (Parity)uiComboBox5.SelectedValue;
+        }
     }
 }
